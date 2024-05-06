020960e88ccf7c3f7b6996e40b052c7ca3899cb8
Ignacio Fagian <nachofagian@gmail.com>
2024-05-06 10:55:04 -0300

    woocommerce docs

--- docs/complementos/woocommerce.md
+++ docs/complementos/woocommerce.md
@@ -2,4 +2,29 @@
 sidebar_position: 1
 ---
 
-# Woocommerce
+# Pagos en Woocommerce
+
+## Ejemplo
+
+Sitio de demostración con plugin Pentagoo Woocommerce
+
+http://web3woocommerce.xyz/
+
+## Paso a paso
+
+### Instalación
+
+En primer lugar debemos descargar los fuentes del plugin pentagoo [pentagoo-plugin.zip](https://cdn.pentagoo.com/#/)
+
+Desde el panel de administración de wordpress ingresamos a la sección Plugins.
+
+Clickeamos en el botón "Agregar nuevo" (Add new)
+
+Clickeamos en el botón "Subir plugin" (Upload plugin)
+
+En el dialogo para seleccionar archivos, seleccionamos el archivo zip del plugin descargado previamente.
+
+Como paso final, debemos de activar el plugin para que el mismo quede activo.
+
+### Configuración
+
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,124 +4,119 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import { themes as prismThemes } from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: 'Pentagoo',
-	tagline: 'Dinosaurs are cool',
-	favicon: 'img/favicon.ico',
+  title: "Pentagoo",
+  tagline: "Dinosaurs are cool",
+  favicon: "img/favicon.ico",
 
-	// Set the production url of your site here
-	url: 'https://pentagoo.github.io',
-	// Set the /<baseUrl>/ pathname under which your site is served
-	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: '/pgoo-docs/',
+  // Set the production url of your site here
+  url: "https://pentagoo.github.io",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/pgoo-docs/",
 
-	// GitHub pages deployment config.
-	// If you aren't using GitHub pages, you don't need these.
-	organizationName: 'pentagoo', // Usually your GitHub org/user name.
-	projectName: 'pgoo-docs', // Usually your repo name.
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "pentagoo", // Usually your GitHub org/user name.
+  projectName: "pgoo-docs", // Usually your repo name.
 
-	onBrokenLinks: 'throw',
-	onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
-	// Even if you don't use internationalization, you can use this field to set
-	// useful metadata like html lang. For example, if your site is Chinese, you
-	// may want to replace "en" with "zh-Hans".
-	i18n: {
-		defaultLocale: 'es',
-		locales: ['en', 'es'],
-	},
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "es",
+    locales: ["en", "es"],
+  },
 
-	presets: [
-		[
-			'classic',
-			/** @type {import('@docusaurus/preset-classic').Options} */
-			({
-				docs: {
-					sidebarPath: './sidebars.js',
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-				},
-				blog: {
-					showReadingTime: true,
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-				},
-				theme: {
-					customCss: './src/css/custom.css',
-				},
-			}),
-		],
-	],
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: "./sidebars.js",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/pentagoo/pgoo-docs/tree/main/docs/",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      }),
+    ],
+  ],
 
-	themeConfig:
-		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-		({
-			// Replace with your project's social card
-			image: 'img/docusaurus-social-card.jpg',
-			navbar: {
-				title: 'Pentagoo',
-				logo: {
-					alt: 'Pgoo logo',
-					src: 'img/pgoo-logo.png',
-				},
-				items: [
-					{
-						type: 'docSidebar',
-						sidebarId: 'tutorialSidebar',
-						position: 'left',
-						label: 'Documentación',
-					},
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: "img/docusaurus-social-card.jpg",
+      navbar: {
+        title: "Pentagoo",
+        logo: {
+          alt: "Pgoo logo",
+          src: "img/pgoo-logo.png",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Documentación",
+          },
 
-					{
-						href: 'https://github.com/pentagoo',
-						label: 'GitHub',
-						position: 'right',
-					},
-				],
-			},
-			footer: {
-				style: 'dark',
-				links: [
-					{
-						title: 'Documentación',
-						items: [
-							{
-								label: 'Documentación',
-								to: '/docs/introduccion',
-							},
-						],
-					},
-					{
-						title: 'Community',
-						items: [
-							{
-								label: 'Stack Overflow',
-								href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-							},
-						],
-					},
-					{
-						title: 'More',
-						items: [
-							{
-								label: 'GitHub',
-								href: 'https://github.com/pentagoo',
-							},
-						],
-					},
-				],
-				copyright: `Copyright © ${new Date().getFullYear()} Pentagoo, Inc. Built with Docusaurus.`,
-			},
-			prism: {
-				theme: prismThemes.github,
-				darkTheme: prismThemes.dracula,
-			},
-		}),
+          {
+            href: "https://github.com/pentagoo",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Documentación",
+            items: [
+              {
+                label: "Documentación",
+                to: "/docs/introduccion",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/pentagoo",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Pentagoo, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
 };
 
 export default config;
