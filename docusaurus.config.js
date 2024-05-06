d03123c3616114fe048022d66bd8c0ef1ee13f5a
Ignacio Fagian <nachofagian@gmail.com>
2024-05-05 21:21:32 -0300

    changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,17 +1,19 @@
 // @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: 'Pentagoo documentation',
-	tagline: 'Payments',
+	title: 'Pentagoo',
+	tagline: 'Dinosaurs are cool',
 	favicon: 'img/favicon.ico',
 
 	// Set the production url of your site here
-	url: 'http://localhost:3000',
+	url: 'https://pentagoo.com',
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: '/',
@@ -24,12 +26,12 @@ const config = {
 	onBrokenLinks: 'throw',
 	onBrokenMarkdownLinks: 'warn',
 
-	// Even if you don't use internalization, you can use this field to set useful
-	// metadata like html lang. For example, if your site is Chinese, you may want
-	// to replace "en" with "zh-Hans".
+	// Even if you don't use internationalization, you can use this field to set
+	// useful metadata like html lang. For example, if your site is Chinese, you
+	// may want to replace "en" with "zh-Hans".
 	i18n: {
-		defaultLocale: 'en',
-		locales: ['en'],
+		defaultLocale: 'es',
+		locales: ['en', 'es'],
 	},
 
 	presets: [
@@ -38,7 +40,7 @@ const config = {
 			/** @type {import('@docusaurus/preset-classic').Options} */
 			({
 				docs: {
-					sidebarPath: require.resolve('./sidebars.js'),
+					sidebarPath: './sidebars.js',
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
 					editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
@@ -50,7 +52,7 @@ const config = {
 					editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
 				},
 				theme: {
-					customCss: require.resolve('./src/css/custom.css'),
+					customCss: './src/css/custom.css',
 				},
 			}),
 		],
@@ -64,19 +66,19 @@ const config = {
 			navbar: {
 				title: 'Pentagoo',
 				logo: {
-					alt: 'Pentagoo',
-					src: 'img/logo.svg',
+					alt: 'Pgoo logo',
+					src: 'img/pgoo-logo.png',
 				},
 				items: [
 					{
 						type: 'docSidebar',
 						sidebarId: 'tutorialSidebar',
 						position: 'left',
-						label: 'Tutorial',
+						label: 'Documentación',
 					},
-					{ to: '/blog', label: 'Blog', position: 'left' },
+
 					{
-						href: 'https://github.com/facebook/docusaurus',
+						href: 'https://github.com/pentagoo',
 						label: 'GitHub',
 						position: 'right',
 					},
@@ -86,11 +88,11 @@ const config = {
 				style: 'dark',
 				links: [
 					{
-						title: 'Docs',
+						title: 'Documentación',
 						items: [
 							{
-								label: 'Tutorial',
-								to: '/docs/intro',
+								label: 'Documentación',
+								to: '/docs/introduccion',
 							},
 						],
 					},
@@ -98,12 +100,8 @@ const config = {
 						title: 'Community',
 						items: [
 							{
-								label: 'Discord',
-								href: 'https://discordapp.com/invite/docusaurus',
-							},
-							{
-								label: 'Twitter',
-								href: 'https://twitter.com/docusaurus',
+								label: 'Stack Overflow',
+								href: 'https://stackoverflow.com/questions/tagged/docusaurus',
 							},
 						],
 					},
@@ -111,23 +109,19 @@ const config = {
 						title: 'More',
 						items: [
 							{
-								label: 'Blog',
-								to: '/blog',
-							},
-							{
 								label: 'GitHub',
-								href: 'https://github.com/pentagoo/pgoo-docs',
+								href: 'https://github.com/pentagoo',
 							},
 						],
 					},
 				],
-				copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+				copyright: `Copyright © ${new Date().getFullYear()} Pentagoo, Inc. Built with Docusaurus.`,
 			},
 			prism: {
-				theme: lightCodeTheme,
-				darkTheme: darkCodeTheme,
+				theme: prismThemes.github,
+				darkTheme: prismThemes.dracula,
 			},
 		}),
 };
 
-module.exports = config;
+export default config;
